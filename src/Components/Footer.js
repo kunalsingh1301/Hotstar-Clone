0fe986dd00c425b8973598317131582f3f351e9e
kunalsingh1301 <kunal.singh@iitg.ac.in>
2023-06-11 20:45:20 +0530

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import '../Styles/Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <div className='l1'>
+        <div className='l2'>
+          <div className='l3'>
+            Company
+            <div className='l4'>
+              <span>About us</span>
+              <span>careers</span>
+            </div>
+          </div>
+          <div className='l3'>
+            View Website in
+            <div className='l4'>
+              <span>&#10003; English</span>
+            </div>
+          </div>
+          <div className='l3'>
+            Need Help?
+            <div className='l4'>
+              <span>View Help Centre</span>
+              <span>Share Feedback</span>
+            </div>
+          </div>
+          <div className='l3'>
+            Connect wit us
+            <div className='l4'>
+              <span>f</span>
+              <span>t</span>
+
+            </div>
+          </div>
+        </div>
+        <div className='l22'>
+          <span>&#169; 2023 STAR All Rights Reserved</span>
+          <span>Term of Use privacy policy FAQ</span>
+        </div>
+      </div>
+    </>
   )
 }
 
